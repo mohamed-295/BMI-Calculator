783b9a2896426437c5842da7ccc7becf9eba051e
mohamed-295 <138289745+mohamed-295@users.noreply.github.com>
2024-07-23 23:52:14 +0300

    main.dart

---
+++ main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:myapp/CalculatorScreen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp();
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'BMI Calculator',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: BMICalculator(),
+    );
+  }
+}
